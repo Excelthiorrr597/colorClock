574d007cf4e365adab373f125f7432558b91bc62
Jonathon Gregory <JGregory@Jonathons-MacBook-Pro.local>
2015-09-14 19:06:50 -0500

    need to figure out how to make the time change to hexadecimal

---
+++ styles/clock.js
@@ -0,0 +1,34 @@
+
+window.onload = function() {
+
+
+	var time = new Date().toLocaleTimeString()
+
+	var timeStamp = document.getElementById('clock')
+	
+	var time = document.getElementById('time')
+
+	var createEl = document.createElement('script')
+
+	createEl.innerHTML = "00:64:00"
+
+
+
+
+	timeStamp.onmousehover = function() {
+		timeStamp.replaceChild(createEl, time)
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
